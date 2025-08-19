4ba5a6f809b1ef88a1c1dd32f604e7c80d1a36d0
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2025-08-19 16:07:36 +0300

    feat: add earning release document types and related structs

--- backend/src/types/models.rs
+++ backend/src/types/models.rs
@@ -12,52 +12,74 @@ pub struct Institution {
     pub collections: Vec<String>, 
 }
 
-#[derive(CandidType, Deserialize, Clone, Serialize)]
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
 pub struct CollectionMetadata {
     pub institution_id: String, 
     pub collection_id: String, 
     pub owner: Principal,
     pub name: String,
     pub description: Option<String>,
-    pub image_url: Option<String>, // Consider minting it as well
     pub external_url: Option<String>,
     pub created_at: u64,
     pub updated_at: u64,
     pub category: Option<CollectionCategory>,
-    pub documents: Vec<String>, // List of document IDs
+    pub documents: Vec<String>, 
 }
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum CollectionCategory {
-    UniversityGraduationCertificate,
+    EarningRelease,
 }
 
-#[derive(CandidType, Deserialize, Clone, Serialize)]
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
 pub struct Document {
+    pub institution_id: Option<String>, 
     pub collection_id: Option<String>, 
     pub document_id: String,
     pub owner: Principal,
     pub name: String,
     pub description: Option<String>,
-    pub image_url: Option<String>,
     pub document_hash: String,
+    pub document_data: DocumentType,
     pub file_size: u64,        
     pub file_type: String,    
     pub file_data: Option<Vec<u8>>,     
-    pub recipient: Option<RecipientInfo>
 }
-
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
-pub struct RecipientInfo {
-    pub name: String,
-    pub id: Option<String>,
-    pub email: Option<String>,
+pub enum DocumentType {
+    EarningRelease(EarningReleaseData),
 }
 
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
+pub struct EarningReleaseData {
+    pub document_id: String,
+    pub earning_release_id: String,
+    pub quarter: u8,
+    pub year: u16,
+    pub consolidated_income_data: ConsolidatedIncomeData,
+    pub consolidated_balance_sheet_data: ConsolidatedBalanceSheetData,
+}
+
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
+pub struct ConsolidatedIncomeData {
+    pub gross_profit: f64,
+    pub operating_profit: f64,
+    pub ebitda: f64,
+    pub profit_before_tax: f64,
+    pub net_profit: f64,
+}
+
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
+pub struct ConsolidatedBalanceSheetData {
+    pub total_assets: f64,
+    pub total_equity: f64,
+    pub total_liabilities: f64,
+    pub total_liabilities_and_equity: f64,
+}
 
 // Response type
-#[derive(CandidType, Deserialize, Clone, Serialize)]
+#[derive(CandidType, Deserialize, Clone, Serialize, Debug)]
 pub struct DocumentResponse {
     pub success: bool,
     pub document_id: Option<String>,
